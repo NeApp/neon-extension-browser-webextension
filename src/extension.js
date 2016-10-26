eea036cfa944f843c1483973090c20230f24448b
Dean Gardiner <me@dgardiner.net>
2016-10-27 11:33:19 +1300

    Implemented the `Extension` "installed" event
    EON-16

--- src/extension.js
+++ src/extension.js
@@ -1,8 +1,19 @@
 /* global browser */
 import {Extension} from 'eon.extension.browser.base/extension';
 
+import {isDefined} from 'eon.extension.framework/core/helpers';
+
 
 export class WebExtensionsExtension extends Extension {
+    constructor() {
+        super();
+
+        this._listeners = [];
+
+        // Bind to runtime events
+        this.bind();
+    }
+
     static get supported() {
         return true;
     }
@@ -15,7 +26,66 @@ export class WebExtensionsExtension extends Extension {
         return this.api.id;
     }
 
+    bind() {
+        if(!this.constructor.supported) {
+            return false;
+        }
+
+        // Bind to runtime events
+        this._bind(this.api.onInstalled, (details) =>
+            this.emit('installed', details)
+        );
+
+        // Successfully bound to runtime events
+        return true;
+    }
+
     getUrl(path) {
         return this.api.getURL(path);
     }
+
+    dispose() {
+        // Unbind listeners
+        this._listeners.forEach((entry) => {
+            try {
+                entry.unbind();
+            } catch(e) {
+                console.warn('Unable to unbind %o from event %o', entry.callback, entry.event);
+            }
+        });
+
+        // Reset state
+        this._listeners = [];
+    }
+
+    // region Private methods
+
+    _bind(event, callback) {
+        callback = callback.bind(this);
+
+        // Ensure event exists
+        if(!isDefined(event)) {
+            return;
+        }
+
+        // Bind to event
+        try {
+            event.addListener(callback);
+        } catch(e) {
+            console.warn('Unable to bind %o to event %o:', callback, event, e.stack);
+            return;
+        }
+
+        // Store reference (for future disposal)
+        this._listeners.push({
+            event: event,
+            callback: callback,
+
+            unbind: () => {
+                event.removeListener(callback);
+            }
+        });
+    }
+
+    // endregion
 }

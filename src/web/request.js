cb0d870203374bdd2b9195c9a1311563650c89b4
Dean Gardiner <me@dgardiner.net>
2018-01-22 18:01:47 +1300

    Added the `WebExtensionsWebRequest.api` property

--- src/web/request.js
+++ src/web/request.js
@@ -4,6 +4,10 @@ import {WebExtensionsWebRequestEvent} from './requestEvent';
 
 
 export class WebExtensionsWebRequest extends WebRequest {
+    static get api() {
+        return browser.webRequest;
+    }
+
     static get key() {
         return 'webRequest';
     }
